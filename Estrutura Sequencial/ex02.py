601088abee15ad1ea5475641b49621b678cfc053
adrianowindson <adrianowindsonbjj@gmail.com>
2022-01-31 20:26:40 -0300

    ;*

---
+++ Estrutura Sequencial/ex02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+numero=(input('Digite um número: '))
+print('O numero informado foi: ' + numero)
