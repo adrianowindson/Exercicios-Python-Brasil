f7da2e0e4a83b8f0531de512016e3a6d0d76f866
adrianowindson <adrianowindsonbjj@gmail.com>
2022-01-31 20:47:49 -0300

    ;*

---
+++ Estrutura Sequencial/ex03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#Faça um Programa que peça dois números e imprima a soma. 
+
+n1=int(input('Digite um primeiro número: '))
+n2=int(input('Digite outro número: '))
+
+soma = n1 + n2
+print('O resultado da soma é: %d' %soma)
