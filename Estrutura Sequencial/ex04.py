4dfa73ab1c3c3060dbc21b2d5d0998b2418ae52a
adrianowindson <adrianowindsonbjj@gmail.com>
2022-02-14 22:02:50 -0300

    ;*

---
+++ Estrutura Sequencial/ex04.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+n1=float(input('Digite a nota do primeiro bimestre: '))
+n2=float(input('Digite a nota do segundo bimestre: '))
+n3=float(input('Digite a nota do terceiro bimestre: '))
+n4=float(input('Digite a nota do quarto bimestre: '))
+
+media = (n1 + n2 + n3 + n4) / 4
+print('Media final é igual á: %f' %media)
