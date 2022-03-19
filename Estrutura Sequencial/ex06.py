d44094336e85e986dc742b8a84eb91ba7330a8f0
adrianowindson <adrianowindsonbjj@gmail.com>
2022-03-19 11:13:46 -0300

    =)

---
+++ Estrutura Sequencial/ex06.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+r = float(input('Digite o raio do círculo: '))
+pi = 3.1415
+area = r**2 * pi
+print('A área do Círculo é: %f' %area)
