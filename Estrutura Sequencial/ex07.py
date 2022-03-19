102d8258d0126ed577af6166c6651812fad0cc52
adrianowindson <adrianowindsonbjj@gmail.com>
2022-03-19 11:29:50 -0300

    :)

---
+++ Estrutura Sequencial/ex07.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+
+ladoQuadrado = float(input('Qual o lado do quadrado: '))
+a = ladoQuadrado**2
+print('A área do quadrado é: %f' %a)
