6e47ade9ccfa188992d3f90871e0706b5258e68a
adrianowindson <adrianowindsonbjj@gmail.com>
2022-03-18 23:33:01 -0300

    ;*

---
+++ Estrutura Sequencial/ex05.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env/ python3
+
+#Faça um Programa que converta metros para centímetros. 
+
+metros=int(input('Digite o valor em metros: '))
+cm=metros * 100
+print('O valor em cm é:%i' %cm)
